3a34c8896e6119079c5cd2b4b56db1ee1cc1725e
Jose F. Quiroga <josefelipeq@live.com>
2019-11-28 13:57:38 -0500

    ahora si

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -77,10 +77,7 @@ class ContactForm extends Component {
             placeholder={language === "en" ? "Message" : "Mensaje"}
           ></textarea>
         </div>
-        <div
-          className="form-button-container"
-          style={{ display: "flex", justifyContent: "center" }}
-        >
+        <div className="form-button-container">
           {!this.props.allowSubmit ? (
             <LoaderSpinner />
           ) : (
